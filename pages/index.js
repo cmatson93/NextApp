259f8491e076b18017490821447bf989321c8cdd
Christina Matson <cmatson93@gmail.com>
2018-11-03 21:19:07 -0700

    url creation

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-// This is the Link API
+import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
-const Index = () => (
-  <div>
-    <Link href="/about" >
-            <button>Go to About Page</button>
-    </Link>
-    <p>Hello Next.js</p>
-  </div>
-);
+const PostLink = (props) => (
+    <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
-export default Index
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+);
